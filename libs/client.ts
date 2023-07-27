3f67344d7f3d021b133b770bbb406750abee4cb6
Mochi <beeyugo55@gmail.com>
2023-07-27 23:54:16 +0900

    commit11

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,7 @@
 // libs/client.js
 import { createClient } from 'microcms-js-sdk';
 
-// 環境変数 API_KEY の値を取得
-const apiKey = process.env.API_KEY;
-
 export const client = createClient({
   serviceDomain: 'pvscreeningfes',
-  apiKey: apiKey,
+  apiKey: process.env.API_KEY,
 });
