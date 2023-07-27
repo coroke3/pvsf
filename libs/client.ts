86b36581cefff5a5bedb16aa4daab687cc3c074d
Mochi <beeyugo55@gmail.com>
2023-07-27 19:43:33 +0900

    commit3

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,18 @@
 // libs/client.js
 import { createClient } from 'microcms-js-sdk';
 
-export const client = createClient({
-  serviceDomain: 'pvscreeningfes',
-  apiKey: process.env.API_KEY,
-});
+// APIキーが文字列であることを保証してからcreateClientを呼び出す
+const apiKey = process.env.API_KEY;
+
+let client;
+if (typeof apiKey === 'string') {
+  client = createClient({
+    serviceDomain: 'pvscreeningfes',
+    apiKey: apiKey,
+  });
+} else {
+  // APIキーが未定義または文字列ではない場合のエラー処理を行う
+  throw new Error('API_KEY is not defined or not a valid string in the environment variables.');
+}
+
+export default client;
