9bb32a261ca7a5b69ba507b2fdd4a78f8829d69a
Mochi <beeyugo55@gmail.com>
2023-07-28 15:51:36 +0900

    commit18

--- pages/_documet.js
+++ pages/_documet.js
@@ -1,3 +1,4 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 class SampleDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
