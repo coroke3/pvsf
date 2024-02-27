05051768876a4dde4428ccc9158134c477e4f3c0
coroke3 <beeyugo55@gmail.com>
2024-02-27 23:16:39 +0900

    commit81

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,26 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://pvsf.com",
+  siteUrl: "https://pvsf.jp",
   generateRobotsTxt: true,
-  exclude: ["/server-sitemap-index.xml"], // <= exclude here
+  exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      "https://pvsf.com/server-sitemap-index.xml", // <==== Add here
+      "https://pvsf.jp/server-sitemap.xml", // <==== Add here
     ],
-  },
-  transform: async (config, path) => {
-    return {
-      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      images: [{ loc: "https://example.com/image.jpg" }],
-      news: {
-        title: "Article 1",
-        publicationName: "Google Scholar",
-        publicationLanguage: "en",
-        date: new Date(),
-      },
-    };
   },
 };
