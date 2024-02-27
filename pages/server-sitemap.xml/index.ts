17180bdbfef8b7dadade7e73c4742c5887a4c7e8
coroke3 <beeyugo55@gmail.com>
2024-02-27 23:34:14 +0900

    commit82

---
+++ pages/server-sitemap.xml/index.ts
@@ -0,0 +1,35 @@
+// pages/server-sitemap.xml.ts
+
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // サイトマップのデータを生成
+  const sitemapData = [
+    { url: 'https://pvsf.jp/', lastmod: new Date().toISOString() },
+    { url: 'https://pvsf.jp/blog', lastmod: new Date().toISOString() }
+  ];
+
+  // XML形式のサイトマップを構築
+  const xml = buildSitemapXml(sitemapData);
+
+  // サイトマップをレスポンスとして返す
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(xml);
+  res.end();
+}
+
+// サイトマップデータをXML形式に変換する関数
+function buildSitemapXml(sitemapData: { url: string, lastmod: string }[]) {
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  
+  sitemapData.forEach(({ url, lastmod }) => {
+    xml += `<url>\n`;
+    xml += `<loc>${url}</loc>\n`;
+    xml += `<lastmod>${lastmod}</lastmod>\n`;
+    xml += `</url>\n`;
+  });
+
+  xml += '</urlset>';
+  return xml;
+}
