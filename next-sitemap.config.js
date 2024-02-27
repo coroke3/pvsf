465168a5120205ac44a59044d724de562c4046ad
coroke3 <beeyugo55@gmail.com>
2024-02-27 23:07:35 +0900

    commit80

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,4 +8,19 @@ module.exports = {
       "https://pvsf.com/server-sitemap-index.xml", // <==== Add here
     ],
   },
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      images: [{ loc: "https://example.com/image.jpg" }],
+      news: {
+        title: "Article 1",
+        publicationName: "Google Scholar",
+        publicationLanguage: "en",
+        date: new Date(),
+      },
+    };
+  },
 };
