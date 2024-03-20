6c08c22572daff650488c4ce1e32cac620fdc738
Mochi <beeyugo55@gmail.com>
2024-03-20 22:34:00 +0900

    commit100

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -1,6 +1,13 @@
 // pages/server-sitemap-index.xml/index.tsx
 import { getServerSideSitemapIndexLegacy } from "next-sitemap";
 import { GetServerSideProps } from "next";
+
+// 不適切な文字を削除する関数
+function removeInvalidChars(input: string): string {
+  // 不適切な文字を正規表現で置換
+  return input.replace(/[^\x20-\x7E]/g, '');
+}
+
 export const runtime = 'experimental-edge';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
@@ -10,15 +17,14 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   );
   const jsonData: any[] = await response.json(); // jsonData の型を any[] として明示的に指定
 
-  const sitemapItems = jsonData.map((item: any) => ({ // item の型を any として明示的に指定
-    loc: `https://pvsf.jp/work/${item.ylink.slice(17, 28)}`,
+  const sitemapItems = jsonData.map((item: any) => ({
+    // 不適切な文字を削除
+    loc: `https://pvsf.jp/work/${removeInvalidChars(item.ylink.slice(17, 28))}`,
   }));
   
-  const sitemapUrls = sitemapItems.map((item) => item.loc); // loc プロパティを抽出して新しい配列を作成
+  const sitemapUrls = sitemapItems.map((item) => item.loc);
 
   return getServerSideSitemapIndexLegacy(ctx, sitemapUrls);
-  
-  
 };
 
 // Default export to prevent next.js errors
