ab12edc3ffdef663a58295c3f37da5f6bc232a4c
coroke3 <beeyugo55@gmail.com>
2025-03-06 16:51:23 +0900

    投稿予定

--- components/Header.jsx
+++ components/Header.jsx
@@ -36,14 +36,14 @@ function Header() {
       switch (router.pathname) {
         case "/":
           // トップページ: 1画面分スクロールで表示/非表示
-          if (window.scrollY > (window.innerHeight*0.75)) {
+          if (window.scrollY > (window.innerHeight * 0.75)) {
             setIsHide(false);
           } else {
             setIsHide(true);
           }
           break;
 
-       
+
         default:
           // その他のページ: 常に表示
           setIsHide(false);
@@ -56,7 +56,7 @@ function Header() {
     };
 
     // スクロールイベントを監視するパスを指定
-    const shouldWatchScroll = ["/" ].includes(router.pathname);
+    const shouldWatchScroll = ["/"].includes(router.pathname);
     if (shouldWatchScroll) {
       window.addEventListener('scroll', handleScroll);
     } else {
@@ -79,7 +79,7 @@ function Header() {
         }
         break;
 
-     
+
       default:
         // その他のページ: 常に表示
         setIsHide(false);
@@ -101,53 +101,53 @@ function Header() {
     <header className={`site-header ${isHide ? "Hide" : ""}`}>
       <div className="header-content">
         <div className={`logo-area `}>
-        <a href={"../../../"}>
+          <a href={"../../../"}>
 
-          <p className="event-type">movie event</p>
-          <div className="logo-title">
-            <div className="logo">
-              <svg
-                viewBox="0 0 803.48 1905.69"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <rect
-                    x="7.16"
-                    y="458.51"
-                    width="175.36"
-                    height="497.73"
-                  ></rect>
-                  <path
-                    className="cls-1"
-                    d="m83,311.78c0-126.35,102.43-228.78,228.78-228.78s228.78,102.43,228.78,228.78-102.43,228.78-228.78,228.78"
-                  ></path>
-                </g>
-                <g>
-                  <rect
-                    x="480.6"
-                    y="915.85"
-                    width="156.84"
-                    height="488.75"
-                    transform="translate(1719.25 601.21) rotate(90)"
-                  ></rect>
-                  <path d="m314.55,1905.69v-227.62c0-36.65,7.2-72.24,21.39-105.79,13.69-32.37,33.27-61.42,58.2-86.34,24.93-24.93,53.98-44.51,86.34-58.2,33.55-14.19,69.14-21.38,105.79-21.38h217.22v155.57h-217.22c-64.05,0-116.15,52.1-116.15,116.15v227.62h-155.57Z"></path>
-                </g>
-                <g>
-                  <polygon points="508.5 920.75 462.65 1017.19 318.84 683.28 409.61 683.28 508.5 920.75"></polygon>
-                  <circle cx="559.02" cy="733.79" r="50.51"></circle>
-                </g>
-                <g>
-                  <path d="m265.66,1146.44h-77c0-28.81-23.44-52.25-52.25-52.25v-77c71.27,0,129.25,57.98,129.25,129.25Z"></path>
-                  <path d="m136.41,1198.69c-28.81,0-52.25-23.44-52.25-52.25H7.16c0,71.27,57.98,129.25,129.25,129.25,28.81,0,52.25,23.44,52.25,52.25h77c0-71.27-57.98-129.25-129.25-129.25Z"></path>
-                  <path d="m141.51,1457.18c-71.27,0-129.25-57.98-129.25-129.25h77c0,28.81,23.44,52.25,52.25,52.25v77Z"></path>
-                </g>
-              </svg>
+            <p className="event-type">movie event</p>
+            <div className="logo-title">
+              <div className="logo">
+                <svg
+                  viewBox="0 0 803.48 1905.69"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g>
+                    <rect
+                      x="7.16"
+                      y="458.51"
+                      width="175.36"
+                      height="497.73"
+                    ></rect>
+                    <path
+                      className="cls-1"
+                      d="m83,311.78c0-126.35,102.43-228.78,228.78-228.78s228.78,102.43,228.78,228.78-102.43,228.78-228.78,228.78"
+                    ></path>
+                  </g>
+                  <g>
+                    <rect
+                      x="480.6"
+                      y="915.85"
+                      width="156.84"
+                      height="488.75"
+                      transform="translate(1719.25 601.21) rotate(90)"
+                    ></rect>
+                    <path d="m314.55,1905.69v-227.62c0-36.65,7.2-72.24,21.39-105.79,13.69-32.37,33.27-61.42,58.2-86.34,24.93-24.93,53.98-44.51,86.34-58.2,33.55-14.19,69.14-21.38,105.79-21.38h217.22v155.57h-217.22c-64.05,0-116.15,52.1-116.15,116.15v227.62h-155.57Z"></path>
+                  </g>
+                  <g>
+                    <polygon points="508.5 920.75 462.65 1017.19 318.84 683.28 409.61 683.28 508.5 920.75"></polygon>
+                    <circle cx="559.02" cy="733.79" r="50.51"></circle>
+                  </g>
+                  <g>
+                    <path d="m265.66,1146.44h-77c0-28.81-23.44-52.25-52.25-52.25v-77c71.27,0,129.25,57.98,129.25,129.25Z"></path>
+                    <path d="m136.41,1198.69c-28.81,0-52.25-23.44-52.25-52.25H7.16c0,71.27,57.98,129.25,129.25,129.25,28.81,0,52.25,23.44,52.25,52.25h77c0-71.27-57.98-129.25-129.25-129.25Z"></path>
+                    <path d="m141.51,1457.18c-71.27,0-129.25-57.98-129.25-129.25h77c0,28.81,23.44,52.25,52.25,52.25v77Z"></path>
+                  </g>
+                </svg>
+              </div>
+              <div className="title">
+                <h1>PVSF</h1>
+                <p>映像連続投稿祭</p>
+              </div>
             </div>
-            <div className="title">
-              <h1>PVSF</h1>
-              <p>映像連続投稿祭</p>
-            </div>
-          </div>
           </a>
         </div>
 
--- pages/release/index.js
+++ pages/release/index.js
@@ -4,6 +4,7 @@ import Footer from "../../components/Footer";
 import styles from "../../styles/release.module.css";
 import { css } from "@emotion/react";
 import Head from "next/head";
+import { useState } from "react";
 
 export const getStaticProps = async () => {
   const res = await fetch(
@@ -17,6 +18,34 @@ export const getStaticProps = async () => {
 };
 
 export default function Releases(data) {
+  const [isListView, setIsListView] = useState(false);
+
+  // 日付でグループ化する関数
+  const groupByDate = (releases) => {
+    return releases.reduce((groups, release) => {
+      const date = release.data;
+      if (!groups[date]) {
+        groups[date] = [];
+      }
+      groups[date].push(release);
+      return groups;
+    }, {});
+  };
+
+  // リリースを日付でグループ化
+  const groupedReleases = groupByDate(data.release);
+
+  const ViewToggle = () => (
+    <div className={styles.viewToggle}>
+      <button 
+        onClick={() => setIsListView(!isListView)}
+        className={styles.toggleButton}
+      >
+        {isListView ? "カード表示" : "リスト表示"}
+      </button>
+    </div>
+  );
+
   return (
     <div>
       <Head>
@@ -43,161 +72,160 @@ export default function Releases(data) {
         />
       </Head>
       <div className="content">
-        <div className={styles.table}>
-          {data.release.map((release) => {
-            const showYlink =
-              release.ylink !== undefined && release.ylink !== "";
-            return (
-              <div className={styles.tab} key={release.id}>
-                {showYlink ? (
-                  <div
-                    className={styles.releases1}
-                    style={{
-                      backgroundImage: `url(https://i.ytimg.com/vi/${release.ylink.slice(
-                        17,
-                        28
-                      )}/maxresdefault.jpg)`,
-                    }}
-                  >
-                    <div className={styles.releases2}>
-                      <div className={styles.r0}>{release.data}</div>
-                      <div
-                        className={styles.r1}
-                        id="generated-id-1690476115475-vx3fsggdf"
-                      >
-                        {release.time}
-                      </div>
-                      <div
-                        className={styles.r2}
-                        id="generated-id-1690476115475-us5y3bfp6"
-                      >
-                        {release.type2}
-                      </div>
-                      <div className={styles.r3}>
-                        <a
-                          href={`https://twitter.com/${release.tlink}`}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <div
-                            className={styles.r31}
-                            style={{
-                              backgroundImage: `url(https://lh3.googleusercontent.com/d/${release.icon.slice(
-                                33
-                              )})`,
-                            }}
-                          >
-                            <img src="https://i.gyazo.com/dc3cc7d76ef8ce02789baf16df939178.png" />
-                          </div>
-                        </a>
-                      </div>
-                      <div
-                        className={styles.r4}
-                        id="generated-id-1690476115475-e07u3mmo7"
-                      >
-                        {release.creator}
-                      </div>
-                      <div
-                        className={styles.r5}
-                        id="generated-id-1690476115475-bu15q8iql"
-                      >
-                        {release.title}
-                      </div>
-                      <div
-                        className={styles.r6}
-                        id="generated-id-1690476115475-gw648oy86"
-                      >
-                        {release.comment}
-                      </div>
-                      <div className={styles.r7}>
-                        <div className={styles.r71}>
-                          {" "}
-                          <a
-                            href={`https://youtu.be/${release.ylink.slice(
-                              17,
-                              28
-                            )}?list=PLhxvXoQxAfWJu5MXy1QxLuv_RHf_lDsFV`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            id="generated-id-1690507402817-hylf4ea9j"
-                          >
-                            {" "}
-                            YouTubeで視聴する
-                          </a>
+        <ViewToggle />
+        <div className={`${styles.table} ${isListView ? styles.listView : ''}`}>
+          {Object.entries(groupedReleases).map(([date, releases]) => (
+            <div key={date} className={styles.dateGroup}>
+              {isListView && (
+                <div className={styles.dateHeader}>
+                  {date}
+                </div>
+              )}
+              {releases.map((release) => {
+                const showYlink =
+                  release.ylink !== undefined && release.ylink !== "";
+                const iconUrl = `https://lh3.googleusercontent.com/d/${release.icon.slice(33)}`;
+
+                // メンバー情報の取得
+                const members = release.member ? release.member.split(',') : []; // カンマ区切りでメンバーを取得
+                const memberIds = release.memberid ? release.memberid.split(',') : []; // カンマ区切りでメンバーIDを取得
+
+                return (
+                  <div className={`${styles.tab} ${isListView ? styles.listItem : ''}`} key={release.id}>
+                    {isListView ? (
+                      <>
+                      <div className={styles.listContent}>
+                        <img src={iconUrl} alt={release.title} className={styles.icon} />
+                        <span className={styles.date}>{release.time}</span>
+                        <span className={`${styles.types} `}>
+                        <div className={`${styles.type}  ${styles[release.type1]} `}>              {release.type1}</div>
+                        <div className={`${styles.type}  ${styles[release.type2]}`}>              {release.type2}</div>
+                        </span>
+                        <span className={styles.listCreator}>{release.creator}</span>
+                        <span className={styles.listTitle}>{release.title}</span>
                         </div>
-                        <div className={styles.r72}>
+                        <div className={styles.listContent2}>
+                        <span className={styles.listComment}>{release.comment}</span>
+
+                        {/* メンバー一覧の表示 */}
+                        {members.length > 0 && (
+                          <span className={styles.members}>
+                            メンバー: {members.map((member, index) => {
+                              const memberId = memberIds[index] ? memberIds[index].trim() : null; // メンバーIDを取得
+                              return memberId ? (
+                                <span key={index}>
+                                  <a href={`https://x.com/${memberId}`} target="_blank" rel="noopener noreferrer">
+                                    {member.trim()}
+                                  </a>
+                                  {index < members.length - 1 && ', '} {/* カンマ区切り */}
+                                </span>
+                              ) : (
+                                <span key={index}>{member.trim()}{index < members.length - 1 && ', '}</span>
+                              );
+                            })}
+                          </span>
+                        )}
+
+                        <div className={styles.listActions}>
+                          {showYlink && (
+                            <a
+                              href={`https://youtu.be/${release.ylink.slice(17,28)}?list=PLhxvXoQxAfWJu5MXy1QxLuv_RHf_lDsFV`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              視聴
+                            </a>
+                          )}
                           <Link href={`release/${release.timestamp}`}>
-                            詳細を見る
+                            詳細
                           </Link>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  <div className={styles.releases1}>
-                    <div className={styles.releases2}>
-                      <div className={styles.r0}>{release.data}</div>
-                      <div
-                        className={styles.r1}
-                        id="generated-id-1690476115475-vx3fsggdf"
-                      >
-                        {release.time}
-                      </div>
-                      <div
-                        className={styles.r2}
-                        id="generated-id-1690476115475-us5y3bfp6"
-                      >
-                        {release.type2}
-                      </div>
-                      <div className={styles.r3}>
-                        <a
-                          href={`https://twitter.com/${release.tlink}`}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
+                        </div>
+                      </>
+                    ) : (
+                      <div className={styles.releases1} style={{
+                        backgroundImage: showYlink ? 
+                          `url(https://i.ytimg.com/vi/${release.ylink.slice(17,28)}/maxresdefault.jpg)` : 
+                          'none'
+                      }}>
+                        <div className={styles.releases2}>
+                          <div className={styles.r0}>{release.data}</div>
                           <div
-                            className={styles.r31}
-                            style={{
-                              backgroundImage: `url(https://lh3.googleusercontent.com/d/${release.icon.slice(
-                                33
-                              )})`,
-                            }}
+                            className={styles.r1}
+                            id="generated-id-1690476115475-vx3fsggdf"
                           >
-                            <img src="https://i.gyazo.com/dc3cc7d76ef8ce02789baf16df939178.png" />
+                            {release.time}
                           </div>
-                        </a>
-                      </div>
-                      <div
-                        className={styles.r4}
-                        id="generated-id-1690476115475-e07u3mmo7"
-                      >
-                        {release.creator}
-                      </div>
-                      <div
-                        className={styles.r5}
-                        id="generated-id-1690476115475-bu15q8iql"
-                      >
-                        {release.title}
-                      </div>
-                      <div
-                        className={styles.r6}
-                        id="generated-id-1690476115475-gw648oy86"
-                      >
-                        {release.comment}
-                      </div>
-                      <div className={styles.r7}>
-                        <div className={styles.r72}>
-                          <Link href={`release/${release.timestamp}`}>
-                            詳細を見る
-                          </Link>
+                          <div
+                            className={styles.r2}
+                            id="generated-id-1690476115475-us5y3bfp6"
+                          >
+                            {release.type2}
+                          </div>
+                          <div className={styles.r3}>
+                            <a
+                              href={`https://twitter.com/${release.tlink}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <div
+                                className={styles.r31}
+                                style={{
+                                  backgroundImage: `url(${iconUrl})`,
+                                }}
+                              >
+                                <img src="https://i.gyazo.com/dc3cc7d76ef8ce02789baf16df939178.png" />
+                              </div>
+                            </a>
+                          </div>
+                          <div
+                            className={styles.r4}
+                            id="generated-id-1690476115475-e07u3mmo7"
+                          >
+                            {release.creator}
+                          </div>
+                          <div
+                            className={styles.r5}
+                            id="generated-id-1690476115475-bu15q8iql"
+                          >
+                            {release.title}
+                          </div>
+                          <div
+                            className={styles.r6}
+                            id="generated-id-1690476115475-gw648oy86"
+                          >
+                            {release.comment}
+                          </div>
+                          <div className={styles.r7}>
+                            <div className={styles.r71}>
+                              {" "}
+                              <a
+                                href={`https://youtu.be/${release.ylink.slice(
+                                  17,
+                                  28
+                                )}?list=PLhxvXoQxAfWJu5MXy1QxLuv_RHf_lDsFV`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                id="generated-id-1690507402817-hylf4ea9j"
+                              >
+                                {" "}
+                                YouTubeで視聴する
+                              </a>
+                            </div>
+                            <div className={styles.r72}>
+                              <Link href={`release/${release.timestamp}`}>
+                                詳細を見る
+                              </Link>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    )}
                   </div>
-                )}
-              </div>
-            );
-          })}
+                );
+              })}
+            </div>
+          ))}
         </div>
         <Footer />
       </div>
