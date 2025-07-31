e7e6ef9ff95b620f5a182f833caf85d7132f51fa
coroke3 <beeyugo55@gmail.com>
2025-07-31 21:40:02 +0900

    デザイン修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,33 +1,18 @@
 // components/Layout.tsx
 import { ReactNode } from 'react';
 import Header from './Header';
-import { useRouter } from 'next/router';
-import WorksSidebar from './WorksSidebar';
-
-interface Work {
-    id: string;
-    timestamp: string;
-    creator: string;
-    title: string;
-    icon?: string;
-}
 
 interface LayoutProps {
     children: ReactNode;
-    works?: Work[];
 }
 
-export default function Layout({ children, works }: LayoutProps) {
-    const router = useRouter();
-    const isReleasePage = router.pathname === '/release/[id]';
-
+export default function Layout({ children }: LayoutProps) {
     return (
         <>
             <Header />
             <main>
                 {children}
             </main>
-            {isReleasePage && <WorksSidebar works={works} currentId={router.query.id?.toString()} />}
         </>
     );
 }
