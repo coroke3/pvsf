6f7efc7b2adcd89d513f52c2d70791028ceeb885
Mochi <59435515+coroke3@users.noreply.github.com>
2026-02-05 15:49:42 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
   images: {
-    domains: ["i.gyazo.com"], // ここに画像のホスト名を追加します
-  }
+    domains: ["i.gyazo.com"],
+  },
+  // Cloudflare Workers (workerd) で解決できないパッケージを外部化
+  serverExternalPackages: [
+    "jose",
+    "@panva/hkdf",
+    "react-textarea-autosize",
+    "@emotion/react",
+  ],
 };
+
+// Cloudflare Pages ローカル開発用
+const initOpenNextCloudflareForDev = require("@opennextjs/cloudflare").initOpenNextCloudflareForDev;
+initOpenNextCloudflareForDev();
+
+module.exports = nextConfig;
