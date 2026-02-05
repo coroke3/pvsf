664ba07b45adb4743f179df4413df96a008c5e7f
Mochi <59435515+coroke3@users.noreply.github.com>
2026-02-05 15:58:22 +0900

    削除

--- sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-    siteUrl: `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`,
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  };
