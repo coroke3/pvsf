574bf818b4c49ae72a3cea14d6aefd81b2c0d5de
Mochi <59435515+coroke3@users.noreply.github.com>
2026-08-15 14:50:55 +0900

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const isExternalLink = (href) => {
 };
 
 const menuItems = [
-  { title: "PVSF2026Sp", subtitle: "企画概要", href: "../../page/pvsf2026sp" },
+  { title: "PVSF2026S", subtitle: "企画概要", href: "../../page/pvsf2026s" },
   { title: "JOIN", subtitle: "参加する", href: "../../page/join" },
   { title: "RELEASES", subtitle: "投稿予定のご案内", href: "../../release" },
   { title: "Q&A", subtitle: "質問", href: "../../page/qanda" },
